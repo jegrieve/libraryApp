edad107c05efd06604a8e590e3eb9e6ee16e4f43
James Grieve <pocojames@hotmail.com>
2020-10-30 18:06:34 -0700

    complete app logic

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function addBookToLibrary() {
     [author, title, numPages, read] = getInputs();
     let newBook = new Book(author, title, numPages, read);
     myLibrary.push(newBook);
+    updateBooks();
 };
 {/* <input type = "text" id = "author-input" placeholder = "Author">
 <input type = "text" id = "title-input" placeholder = "Title">
@@ -57,6 +58,69 @@ function resetInputs(author, title, pages, read) {
     pages.value = "";
     read.checked = false;
 }
+
+// let btn = document.createElement("button")
+// btn.innerHTML = "Delete"
+// btn.classList.add("delete-btn")
+
+// let btn2 = document.createElement("button")
+// btn2.innerHTML = "Delete2"
+// btn2.classList.add("delete-btn")
+
+function updateBooks() {
+    document.querySelector(".cards").innerHTML = "";
+    myLibrary.forEach((book, i) => {
+        let newCard = document.createElement("div");
+        newCard.classList.add("book-card")
+        document.querySelector(".cards").appendChild(newCard)
+        newCard.id = i;
+        newCard.innerHTML = formatBook(book);
+        let deleteBtn = makeDeleteButton();
+        let toggleReadBtn = makeToggleReadButton();
+        newCard.appendChild(deleteBtn)
+        newCard.appendChild(toggleReadBtn)
+    });
+};
+
+function formatBook(book) {
+    let newCardText = document.createElement("p");
+    let br = document.createElement("br")
+    let readStatus = book.read ? "Read" : "Not Read"
+    newCardText.innerHTML = `Author: ${book.author} <br> <br> Title: ${book.title} <br> <br>  Pages:${book.numPages} <br> <br>  ${readStatus} <br> <br>`
+    return newCardText.innerHTML
+};
+
 //Function that loops through myLibrary array
 //It will display each book to the page
 //
+
+function makeDeleteButton() {
+    let button = document.createElement("button")
+    button.innerHTML = "Delete"
+    button.addEventListener("click", deleteBook)
+    return button
+};
+
+function makeToggleReadButton() {
+    let button = document.createElement("button")
+    button.innerHTML = "Toggle"
+    button.addEventListener("click", toggleBook)
+    return button
+};
+
+function deleteBook(e) {
+    let bookId = e.target.parentNode.id
+    myLibrary.splice(bookId, 1);
+    updateBooks();
+};
+
+function toggleBook(e) {
+    let bookId = e.target.parentNode.id
+    
+    if (myLibrary[bookId].read=== true) {
+        myLibrary[bookId].read = false
+    } else {
+        myLibrary[bookId].read= true
+    }
+    updateBooks();
+};
